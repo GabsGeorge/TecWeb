6506d09ab2f1edcbe4c36fa0d6226f8b259d5386
Gabriel George <gabriel.g.s.santos@hotmail.com>
2018-09-05 09:51:22 -0300

    up

--- EasyParty/settings.py
+++ EasyParty/settings.py
@@ -199,3 +199,14 @@ SOCIAL_AUTH_FACEBOOK_SECRET = '898048350911-sh9pnf1b8vrvpg0fu38n3n2o5n2m4tgb.app
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/configuracoes/'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/configuracoes/'
 SOCIAL_AUTH_RAISE_EXCEPTIONS = False
+
+
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'social_core.backends.github.GithubOAuth2',  # for Github authentication
+ 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+ 
+ 'django.contrib.auth.backends.ModelBackend',
+)
