e225852d7d8cf20d72faa15165154ca3bd6d8e3a
Gabriel George <gabriel.g.s.santos@hotmail.com>
2018-09-26 10:15:46 -0300

    up

--- EasyParty/urls.py
+++ EasyParty/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     url(r'^admin/', admin.site.urls),
-    url(r'^index/admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^index/', views.index, name="index"),
     
--- paineldecontrole/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PaineldecontorleConfig(AppConfig):
-    name = 'paineldecontrole'
