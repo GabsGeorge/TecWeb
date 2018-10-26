9690306307c31c791b32b71a2c874cc68a80c2d8
Gabriel George <gabriel.g.s.santos@hotmail.com>
2018-10-26 04:00:27 -0300

    up

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from localflavor.br.forms import BRZipCodeField
 from .models import Usuario
 
 class UserAdminCreationForm(UserCreationForm):
